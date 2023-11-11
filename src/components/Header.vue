391db59a4b53f3f1c7a0b9faa125b6cf876b43a0
Konrad Nowak <wk.k.nowak@gmail.com>
2023-11-11 21:32:16 +0100

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+<template>
+  <header>
+    <h1>Chernoff's Faces</h1>
+  </header>
+</template>
+
+<style scoped lang="scss">
+@import './../assets/scss/variables.scss';
+
+h1 {
+  color: $primaryColor;
+}
+</style>
